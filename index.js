83224068a02a6f53c893d6ed9208326c1dd0bbf8
0barmenski <1kiloom@gmail.com>
2022-10-10 23:56:48 +0300

    add: some html

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer((request, response) => {
   request.on('error', (err) => {
@@ -14,4 +15,11 @@ http.createServer((request, response) => {
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end(html);
 	});
+  }
+console.log("Server started on :8080");
 }).listen(8080);
+
+process.on('SIGINT', function () { //on ctrl+c
+	        console.log("\nWebserver closed");
+	        process.exit(); //exit completely
+});
--- station.js
+++ station.js
@@ -1,1 +1,1 @@
-
+console.log("station.js is added");
