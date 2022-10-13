dcfeb30b1d871076a9631e3646e5e081c27c4d31
barmenski <1kiloom@gmail.com>
2022-10-13 23:29:05 +0300

    fix: index.js(server)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+var path = require('path');
 
 http
   .createServer((request, response) => {
@@ -10,19 +11,42 @@ http
     });
     response.on('error', (err) => {
       console.error(err);
+      response.end();
     });
     if (request.url === '/') {
-      fs.readFile('./index.html', 'UTF-8', (err, html) => {
+      fs.readFile('./src/index.html', 'UTF-8', (err, html) => {
+        response.statusCode = 200;
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(html);
       });
+    } else if (request.url.match('.css$')) {
+      console.log(request.url);
+      var cssPath = path.join(__dirname, 'src', request.url);
+      var fileStream = fs.createReadStream(cssPath, 'UTF-8');
+      response.writeHead(200, { 'Content-Type': 'text/css' });
+      fileStream.pipe(response);
+    } else if (request.url.match('.ico$')) {
+      console.log(request.url);
+      var favPath = path.join(__dirname, 'src', request.url);
+      var fileStream = fs.createReadStream(favPath);
+      response.writeHead(200, { 'Content-Type': 'image/x-icon' });
+      fileStream.pipe(response);
+    } else if (request.url.match('.js$')) {
+      console.log(request.url);
+      var jsPath = path.join(__dirname, 'src', request.url);
+      var fileStream = fs.createReadStream(jsPath, 'UTF-8');
+      response.writeHead(200, { 'Content-Type': 'text/javascript' });
+      fileStream.pipe(response);
+    } else {
+      response.writeHead(404, { 'Content-Type': 'text/html' });
+      response.end('No Page Found');
     }
-    console.log('Server started on :8080');
   })
-  .listen(8080);
+  .listen(8080, () => {
+    console.log('Server started on :8080');
+  });
 
 process.on('SIGINT', function () {
-  //on ctrl+c
   console.log('\nWebserver closed');
-  process.exit(); //exit completely
+  process.exit();
 });
