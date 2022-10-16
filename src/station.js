3180311d984703fe8c7df93b2e43b74637f83681
barmenski <1kiloom@gmail.com>
2022-10-16 23:18:35 +0300

    add: station.js

--- src/station.js
+++ src/station.js
@@ -3,6 +3,7 @@ import { Temperature } from './temp';
 class Station {
   constructor() {
     this.temperature = new Temperature();
+    this.isPbFree = true;
     this.powerTop = 0;
     this.powerBottom = 0;
     this.tempChip = this.temperature.getTempChip(
@@ -18,15 +19,79 @@ class Station {
       [210, 183],
       [270, 183],
     ];
-    this.profikePbFree = [
+    this.profilePbFree = [
       [120, 150],
       [210, 219],
       [270, 219],
     ];
+    this.timerId = null;
     this.currTime = 0;
+    this.stepPower = 50;
   }
 
   start() {
-    let workPeriod = setInterval(1000);
+    this.timerId = setInterval(1000, heat);
+  }
+
+  stop() {
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
+  }
+
+  heat() {
+    this.currTime++;
+    if (this.isPbFree) {
+      var preHeatTime = this.profilePbFree[0][0];
+      var preHeatTemp = this.profilePbFree[0][1];
+      var waitTime = this.profilePbFree[1][0];
+      var waitTemp = this.profilePbFree[1][1];
+      var reflowTime = this.profilePbFree[2][0];
+      var reflowTemp = this.profilePbFree[2][1];
+    } else {
+      var preHeatTime = this.profilePb[0][0];
+      var preHeatTemp = this.profilePb[0][1];
+      var waitTime = this.profilePb[1][0];
+      var waitTemp = this.profilePb[1][1];
+      var reflowTime = this.profilePb[2][0];
+      var reflowTemp = this.profilePb[2][1];
+    }
+    while (this.currTime < preHeatTime) {
+      let riseTemp = (preHeatTemp - 0) / (preHeatTime - 0);
+      let prevTemp = this.tempChip;
+
+      this.tempChip = this.temperature.getTempChip(
+        this.powerTop,
+        this.powerBottom
+      );
+      let delta = this.tempChip - prevTemp;
+
+      if (delta < riseTemp && delta != 0) {
+        this.powerBottom =
+          this.powerBottom + this.stepPower * (delta / riseTemp);
+      } else if (delta > riseTemp && delta != 0) {
+        this.powerBottom =
+          this.powerBottom - this.stepPower * (delta / riseTemp);
+      }
+    }
+
+    while (this.currTime > preHeatTime && this.currTime < waitTime) {
+      let riseTemp = (waitTemp - preHeatTemp) / (waitTime - preHeatTime);
+      let prevTemp = this.tempChip;
+
+      this.tempChip = this.temperature.getTempChip(
+        this.powerTop,
+        this.powerBottom
+      );
+      let delta = this.tempChip - prevTemp;
+
+      if (delta < riseTemp && delta != 0) {
+        this.powerBottom =
+          this.powerBottom + this.stepPower * (delta / riseTemp);
+      } else if (delta > riseTemp && delta != 0) {
+        this.powerBottom =
+          this.powerBottom - this.stepPower * (delta / riseTemp);
+      }
+    }
   }
 }
