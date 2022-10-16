a93a033eb452ab13d9493b699f2eabc5629bdef0
barmenski <1kiloom@gmail.com>
2022-10-16 16:28:23 +0300

    add: station.js

--- src/station.js
+++ src/station.js
@@ -1,1 +1,32 @@
-console.log("station.js is added");
+import { Temperature } from './temp';
+
+class Station {
+  constructor() {
+    this.temperature = new Temperature();
+    this.powerTop = 0;
+    this.powerBottom = 0;
+    this.tempChip = this.temperature.getTempChip(
+      this.powerTop,
+      this.powerBottom
+    );
+    this.tempBoard = this.temperature.getTempBoard(
+      this.powerTop,
+      this.powerBottom
+    );
+    this.profilePb = [
+      [120, 150],
+      [210, 183],
+      [270, 183],
+    ];
+    this.profikePbFree = [
+      [120, 150],
+      [210, 219],
+      [270, 219],
+    ];
+    this.currTime = 0;
+  }
+
+  start() {
+    let workPeriod = setInterval(1000);
+  }
+}
--- src/temp.js
+++ src/temp.js
@@ -1,3 +1,38 @@
-//export getTopTemp;
-//export getBottomTemp;
-console.log('temp.js is added');
+let maxPower = 3460; // W
+let maxTempRise = 5; // C/sec
+let oneCelSecTempRise = maxPower / maxTempRise; // power for 1 C/sec
+export class Temperature {
+  constructor(tempEnv = 25, tempChip = 25, tempBoard = 25) {
+    this.tempEnv = tempEnv;
+    this.tempChip = tempChip;
+    this.tempBoard = tempBoard;
+  }
+
+  getTempChip(powerTop, powerBottom) {
+    let deltaTemp = this.tempEnv - this.tempChip;
+    if (powerTop === 0 && powerBottom != 0) {
+      this.tempBoard = getTempBoard(powerTop, powerBottom);
+      if (this.tempBoard < this.tempEnv + 20) {
+        this.tempChip = this.tempEnv;
+      } else {
+        this.tempChip = this.tempBoard - 20;
+      }
+    } else if (powerTop != 0 && powerBottom != 0) {
+      this.tempChip =
+        this.tempChip +
+        (powerTop + powerBottom) / oneCelSecTempRise -
+        deltaTemp / 100;
+    }
+
+    return this.tempChip;
+  }
+
+  getTempBoard(powerTop, powerBottom) {
+    let deltaTemp = this.tempEnv - this.tempBoard;
+    this.tempBoard =
+      this.tempBoard +
+      (powerTop * 0.1 + powerBottom) / oneCelSecTempRise -
+      deltaTemp / 100;
+    return this.tempChip;
+  }
+}
